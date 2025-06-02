8477318d777ffdf7fa657b136a165a93cd5dec6a
AtharvGaur <atharava112@gmail.com>
2025-06-02 18:35:54 +0530

    feat: Initialize React project with routing and components
    - Added main application structure with routing using react-router-dom.
    - Implemented Home, TodoList, Card, Form, and ThemeToggle components.
    - Integrated Tailwind CSS for styling and responsive design.
    - Created a user card component to display user information and posts.
    - Developed a form component for user input with controlled components.
    - Added a Todo application with local storage functionality.
    - Implemented theme toggle functionality for light and dark modes.
    - Included SVG assets for React and Vite logos.
    - Set up Vite configuration for React and Tailwind CSS.

--- src/_components/Card.jsx
+++ src/_components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 function Card({cardData}) {
   const [expanded, setExpanded] = useState(false);
